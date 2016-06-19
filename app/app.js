8aeb1183b5d1ee282abb17e274027d85b8a18e92
보롱킴 <dsdgun@gmail.com>
2016-06-19 22:01:27 +0900

    ui-router 예제 추가

--- app/app.js
+++ app/app.js
@@ -1,8 +1,38 @@
 import angular from 'angular';
 import uiRouter from 'angular-ui-router';
+import indexTpl from 'templates/index.html!ng-template'
+import layoutTpl from 'templates/layout.html!ng-template'
+import aboutTpl from 'templates/about.html!ng-template'
 
+class AppController {
+	constructor(){
+		this.title = '보롱 ~ 월드 index.html';
+	}
+}
+class AboutController {
+	constructor(){
+		this.title = 'about.html';
+	}
+}
 angular.module('app', [uiRouter])
-	.controller('AppController', function($scope) {
-		$scope.title = '보롱 월~~드';
-	});
+	.config(($stateProvider) => {
+		$stateProvider.state('main', {
+			abstract:true,
+			templateUrl:layoutTpl.templateUrl,
+		})
+		.state('main.index', {
+			url:'/',
+			templateUrl:indexTpl.templateUrl,
+			controller:AppController,
+			controllerAs:'vm'
+		})
+		.state('main.about', {
+			url:'/about',
+			templateUrl:aboutTpl.templateUrl,
+			controller:AboutController,
+			controllerAs:'vm'
+		})
+
+	})
+	.controller('AppController', AppController);
 angular.bootstrap(document, ['app']);
---
+++ app/templates/menu.js
@@ -0,0 +1,4 @@
+<div>
+	<a ui-sref="main.index">HOME</a>
+	<a ui-sref="main.about">ABOUT</a>
+</div>
